4fef7e368a2395ad12e4059b8fa3198fa66ba701
Obli <adrianmckay202@gmail.com>
2023-05-29 21:43:41 -0500

    Proper use of the new Input System
    THIS TOOK AN ARM AND LEG.

--- ProjectShadow/Assets/Scripts/Player/PlayerMovement.cs
+++ ProjectShadow/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+
     [SerializeField] private float moveSpeed = 1f;
+
+    [SerializeField] private PlayerInput PlayerAction;
+
+    [SerializeField] private Vector2 movement;
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
-
-        Vector2 movement = new Vector2(horizontalInput, verticalInput) * moveSpeed * Time.deltaTime;
         transform.Translate(movement);
+
+        movement = PlayerAction.actions["Movement"].ReadValue<Vector2>() * moveSpeed * Time.deltaTime;
     }
 }
