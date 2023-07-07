a6af88321ae133d430aee6c4eb1b0a0dab657064
Obli <adrianmckay202@gmail.com>
2023-07-07 03:14:24 -0500

    Update EntityData.cs

--- ProjectShadow/Assets/_Scripts/Classes/Class_Entity/EntityData.cs
+++ ProjectShadow/Assets/_Scripts/Classes/Class_Entity/EntityData.cs
@@ -9,14 +9,18 @@ public class EntityData : ScriptableObject
     public Animator animator;
     public CharacterPortrait portraitsheet;
 
+    [Header("Health")]
     public int maxHealth;
     public int currentHealth;
 
+    [Header("Base Armor")]
     public int basePhysicalArmor;
     public int baseMagicArmor;
 
+    [Header("Base Damage")]
     public int basePhysicaldamage;
     public int baseMagicdamage;
 
+    [Header("Base Speed")]
     public float speed;
 }
